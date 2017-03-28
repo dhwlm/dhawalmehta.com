42d672fd2ab83ff59dad5d364c28324d0e7ff386
dhawal.m <dhawal.m@dv-mac-dhawal.internal.directi.com>
2017-03-28 18:09:42 +0530

    files added - readme changed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Dhawal Mehta
+# Dhawal Mehta | Developer | Guitarist
+
+I am an experienced User Interface Designer & Developer with a demonstrated history of working in the internet industry.
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dhawal Mehta | Developer | Guitarist</title>
+    <meta name="description" content="Dhawal Mehta is an experienced User Interface Designer & Developer with a demonstrated history of working in the internet industry.">
+	  <meta name="keywords" content="UI, CMS, Ecommerce, CodeIgniter, Bootstrap, Chrome Extension, Photoshop, Wordpress, AJAX, Responsive Design, Opencart, HTML5, CSS3, jQuery, Javascript, guitar, PRS, Paul Reed Smith, Line6, line 6, POD HD500, POD, HD500">
+    <!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+    <link rel="shortcut icon" href="img/favicon.png">
+
+  </head>
+  <body>
+
+		<div class="container">
+		  <div class="row">
+		    <div class="col-lg-12 intro-cont">
+          <div class="intro text-center">
+            <img src="img/bg.png" alt="Dhawal Mehta | Developer | Web Designer | Web Developer | Guitarist">
+            I am <div class="intro-text-type"></div>
+          </div>
+          <div class="branding text-center">
+            Dhawal Mehta &copy; 2017 <a href="http://www.dhawalmehta.com" title="Dhawal Mehta">dhawalmehta.com</a>
+          </div>
+		    </div>
+		  </div>
+		</div>
+
+   	<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+    <script src="js/typed.js"></script>
+    <script src="js/common.js"></script>
+
+		<script>
+
+    $(function(){
+        $(".intro-text-type").typed({
+          strings: [ "Dhawal Mehta. ","a Web Developer. ","a Guitarist. "],
+          loop: true,
+          typeSpeed: 0,
+          backDelay: 1000
+        });
+    });
+		</script>
+  </body>
+</html>
