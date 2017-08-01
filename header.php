5e0fcae0019f0e46479856dcaf2950ee76f763e5
dhwlm <>
2017-08-01 19:28:59 +0530

    SEO related og tags updated

--- header.php
+++ header.php
@@ -6,6 +6,37 @@
     <title>Dhawal Mehta | Developer | Guitarist</title>
     <meta name="description" content="Dhawal Mehta is an experienced User Interface Designer & Developer with a demonstrated history of working in the internet industry.">
     <meta name="keywords" content="dhawal, Mehta, UI, CMS, Ecommerce, CodeIgniter, Bootstrap, Chrome Extension, Photoshop, Wordpress, AJAX, Responsive Design, Opencart, HTML5, CSS3, jQuery, Javascript, guitar, PRS, Paul Reed Smith, Line6, line 6, POD HD500, POD, HD500, Dhawal Mehta, Dhawal, Mehta">
+
+
+    <!-- Twitter Cards -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:image" content="http://dhawalmehta.com/img/dhawal-mehta.png">
+
+    <meta name="twitter:title" content="Dhawal Mehta | Developer | Guitarist">
+    <meta name="twitter:description" content="Hello, My name is Dhawal Mehta. I'm a UI/UX Developer & a Web/Tech Enthusiast. I transform thoughts, concepts & ideas into well thought out cross-browser compatible design specs in exchange for Green stuff. :P">
+    <meta name="twitter:creator" content="@https://twitter.com/dhwlm">
+
+    <!-- Open Graph -->
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Dhawal Mehta | Developer | Guitarist">
+    <meta property="og:description" content="Hello, My name is Dhawal Mehta. I'm a UI/UX Developer & a Web/Tech Enthusiast. I transform thoughts, concepts & ideas into well thought out cross-browser compatible design specs in exchange for Green stuff. :P">
+    <meta property="og:url" content="http://dhawalmehta.com/">
+    <meta property="og:site_name" content="Dhawal Mehta | Developer | Guitarist">
+    <meta property="og:image" content="http://dhawalmehta.com/img/dhawal-mehta.png" />
+
+    <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "WebSite",
+      "name": "www.dhawalmehta.com",
+      "alternateName": "dhawalmehta.com",
+      "url": "http://www.dhawalmehta.com",
+      "logo": "http://dhawalmehta.com/img/dhawal-mehta.png"
+    }
+    </script>
+
+
     <link rel="shortcut icon" href="img/favicon.png">
     <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.css" />
