f7409b28d6239be66cbec2bb8e2a5dc16bb9fbe2
dhwlm <dhawalmehta91@gmail.com>
2018-02-08 04:14:38 +0530

    removed bootstrp js

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 
     </div>
 
-    <script src="js/bootstrap.min.js"></script>
+    <!-- <script src="js/bootstrap.min.js"></script> -->
     <script src="js/typed.min.js"></script>
     <script src="js/classie.min.js"></script>
     <script src="js/menu.min.js"></script>
